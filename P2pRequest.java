d38c9446007851ed22a76e152f1078ad7b435907
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-13 11:56:48 -0430

    P2pRequest.java: changed object. Putting client_socket field.

--- P2pRequest.java
+++ P2pRequest.java
@@ -1,18 +1,24 @@
+
+import java.net.Socket;
+
 public class P2pRequest{
     String data;
     int data_size;
     int op_code;
     int hash_id;
+    Socket sock;
 	
     public P2pRequest(){
 	data = "";
 	op_code = 0;
 	hash_id = 0;
+        sock = new Socket();
     }
 	
-    public P2pRequest(String d, int oc, int hi){
+    public P2pRequest(String d, int oc, int hi, Socket sk){
 	data = d;
 	op_code = oc;
 	hash_id = hi;
+        sock = sk;
     }
 }
