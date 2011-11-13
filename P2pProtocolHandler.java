d6505148508a56730b04399fb6beda672cb1d279
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-13 11:55:44 -0430

    P2pProtocolHandler.java: implemented sendSong and changed 	getRequest to append socket in request.

--- P2pProtocolHandler.java
+++ P2pProtocolHandler.java
@@ -8,51 +8,79 @@ public class P2pProtocolHandler{
     private static byte REACHABLE_HEXCODE = 0x2;
     private static byte NULL_HEXCODE      = 0x4;
     private static int  NULL_HASHID       = 0xffffffff;
-
-    private Socket client_socket;
-    private BufferedReader in = null;
-    private PrintWriter out = null;
-
-    public P2pProtocolHandler(Socket cs){
-	client_socket = cs;
-	try{
-	    in = new BufferedReader(new InputStreamReader( client_socket.getInputStream()));
-	    out = new PrintWriter(client_socket.getOutputStream(), true);
-	}
-	catch(IOException e){}
+//    
+//    private static byte S_DOWNLOAD_HEXCODE  = 0x0;
+//    private static byte S_CONSULT_HEXCODE   = 0x1;
+//    private static byte S_REACHABLE_HEXCODE = 0x2;
+//    private static byte S_NULL_HEXCODE      = 0x4;
+//    private static int  S_NULL_HASHID       = 0xffffffff;
+    
+    public P2pRequest getRequest(Socket client_socket){
+        BufferedReader in = null;
+        PrintWriter out = null;
+        int header = 0;
+        int op_code = 0;
+        int hash_id = 0;
+        int data_size = 0;
+        try{
+            // Inicialización de buffers.
+            in = new BufferedReader(new InputStreamReader( client_socket.getInputStream()));
+            out = new PrintWriter(client_socket.getOutputStream(), true);
+            //Se lee el header
+            header = in.read();
+            header = header << 16;
+            header += in.read();
+            op_code = header ^ 0xc0000000;
+            hash_id = header ^ 0x3fffffff;
+            data_size = in.read();
+            data_size = data_size <<16;
+            data_size += in.read();
+        }
+        catch(IOException e){}
+        
+        return new P2pRequest(null,op_code,hash_id,client_socket);
     }
-
-    public P2pRequest getRequest(){
-	int header = 0;
-	int op_code = 0;
-	int hash_id = 0;
-	int data_size = 0;
-	try{
-	    //Se lee el header
-	    header = in.read();
-	    header = header << 16;
-	    header += in.read();
-	    op_code = header ^ 0xc0000000;
-	    hash_id = header ^ 0x3fffffff;
-
-	    data_size = in.read();
-	    data_size = data_size <<16;
-	    data_size += in.read();
-	}
-	catch(IOException e){}
-
-	return new P2pRequest(null,op_code,hash_id);
-    }
-
+    
     public void makeConsult(ConcurrentMap<String,String> SongDB,
-			    ConcurrentMap<Integer,P2pRequest> ConsultDB,
-			    P2pRequest req){
-	
+            ConcurrentMap<Integer,P2pRequest> ConsultDB,
+            P2pRequest req){
+        
     }
-
+    
     public void makeReachable(ConcurrentMap<String,String> NodeDB,
-			      ConcurrentMap<Integer,P2pRequest> ConsultDB,
-			      P2pRequest req){}
-
-    public void sendSong(ConcurrentMap<String,String> SongDB,P2pRequest req){}
+            ConcurrentMap<Integer,P2pRequest> ConsultDB,
+            P2pRequest req){}
+    
+    public void sendSong(ConcurrentMap<String,String> SongDB,P2pRequest req) {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        Socket client_socket = req.sock;
+        try {
+            // Crear buffers
+            in = new BufferedReader(new InputStreamReader( 
+                    client_socket.getInputStream()));
+            out = new PrintWriter(client_socket.getOutputStream(), true);
+            // Obtener información acerca de la canción.
+            // De cuántos bytes es el string nombre ?
+            int nReads = req.data_size/2;
+            char[] buff = new char[nReads];
+            in.read(buff, 0, nReads);
+            String songName = new String(buff);
+            // Buscar canción en base de datos
+            String fileName = SongDB.get(songName);
+            File songFile = new File(fileName);
+            FileInputStream fin = new FileInputStream(songFile);
+            byte fileContent[] = new byte[(int)songFile.length()];
+            fin.read(fileContent);
+            // Preparar datos para mandar por socket
+            String stringContent = new String(fileContent);
+            out.write(stringContent);
+        }
+        catch (FileNotFoundException fnf) {
+            System.out.println("Archivo no encontrado: " + fnf);
+        }
+        catch (IOException e) {
+            System.out.println("Error de I/O: " + e);
+        }
+    }
 }
