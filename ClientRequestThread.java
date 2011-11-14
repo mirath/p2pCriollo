dd8274aa01bec54182ea9e94249e37a34b37e45a
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-13 19:31:08 -0430

    ClientRequestThread.java: simplify constructor and deleted 	useless data structs.

--- ClientRequestThread.java
+++ ClientRequestThread.java
@@ -1,45 +1,35 @@
-import java.util.concurrent.*;
 import java.net.*;
 import java.io.*;
 
 public class ClientRequestThread extends Thread{
-
-    private ConcurrentHashMap<String,String> SongDB;
-    private ConcurrentHashMap<String,String> NodeDB;
-    private ConcurrentHashMap<Integer,P2pRequest> ConsultDB;
     private Socket client_socket;
+    private P2pProtocolHandler p2pHandler;
 
-    public ClientRequestThread(Socket cs,
-			       ConcurrentHashMap<String,String> SDB,
-			       ConcurrentHashMap<String,String> NDB,
-			       ConcurrentHashMap<Integer,P2pRequest> CDB){
+    public ClientRequestThread(Socket cs, P2pProtocolHandler p2ph){
 	client_socket = cs;
-	SongDB = SDB;
-	NodeDB = NDB;
-	ConsultDB = CDB;
+        p2pHandler = p2ph;
     }
 
+    @Override
     public void run(){
-	P2pProtocolHandler p2p_handler = new P2pProtocolHandler();
-	P2pRequest request = p2p_handler.getRequest(client_socket);
+	P2pRequest request = p2pHandler.getRequest(client_socket);
 
 	switch(request.op_code){
 	case 1:
-	    p2p_handler.makeConsult(SongDB,ConsultDB,request);
+	    p2pHandler.makeConsult(request, client_socket);
 	    break;
 	case 2:
-	    p2p_handler.makeReachable(NodeDB,ConsultDB,request);
+	    p2pHandler.makeReachable(request, client_socket);
 	    break;
 	case 3:
 	case 4:
-	    p2p_handler.sendSong(SongDB,request);
+	    p2pHandler.sendSong(request, client_socket);
 	    break;
 	}
-
 	try{
 	    client_socket.close();
 	}
-	catch(IOException e){}
+	catch(IOException e) {}
 	return;
     }
 }
