dea59ea96346dad01e8b4492b736ae80b3847a7a
Lorenzo Fundaró García <lorenzofundaro@yahoo.com>
2011-11-15 16:18:10 -0430

    ConsultThread.java: permite a un nodo hacer consultas 	de manera concurrente al resto de sus vecinos.

---
+++ ConsultThread.java
@@ -0,0 +1,36 @@
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class ConsultThread extends Thread {
+    int pos;
+    String[] result;
+    InetAddress ip;
+    P2pRequest req;
+    int port;
+    P2pProtocolHandler p2pHandler;
+    
+   public ConsultThread(int i, String[] respuesta, InetAddress ip, 
+           P2pRequest req, int port, P2pProtocolHandler pHandler) {
+        pos = i;
+        result = respuesta;
+        this.ip = ip;
+        this.req = req;
+        this.port = port; 
+        this.p2pHandler = pHandler;
+    }
+   
+    @Override
+   public void run() {
+       // Crear conexión con el servidor vecino
+        try { 
+            Socket sok = new Socket(this.ip, this.port);
+            result[pos] = p2pHandler.requestConsult(req, sok);
+            sok.close();
+        }
+        catch(IOException e) {
+            result[pos] = "";
+        }
+    }
+}
